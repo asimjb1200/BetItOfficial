b83c5683ca9363faf4fbb379572498b7252818d4
Asim <ajbp68@mail.umsl.edu>
2020-10-28 13:51:05 -0500

    getting rid of build folder in vc

--- betItServer/build/database_connection/pool.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pool = void 0;
-var _a = require('pg'), Pool = _a.Pool, Client = _a.Client;
-// connecting to the server
-// pooling helps to minimize new connections which are memory intensive, and will instead used cached connections
-exports.pool = new Pool();
-// module.exports = {pool};
