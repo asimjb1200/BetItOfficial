ab8cf4fe42ef54a09d6873c48fa7c7a496d07179
Asim <asimjbrown@gmail.com>
2021-05-24 15:19:44 -0500

    changing how game array is populated

--- betItServer/SportsData/Basketball.ts
+++ betItServer/SportsData/Basketball.ts
@@ -13,6 +13,9 @@ class BasketballData {
         let page: number = 2;
         const totalPages: number = apiResponse.data.meta.total_pages;
 
+        // add the original response's data
+        let gameData: BallDontLieData[] = [...apiResponse.data.data];
+
         // set up each url that I want to hit
         for (let i = 0; page <= totalPages; i++) {
             apiArray[i] = axios.get(this.#mainApi + `games?seasons[]=${year}&per_page=100&page=${page}`);
@@ -21,10 +24,7 @@ class BasketballData {
         let apiResponseData = await Promise.all(apiArray);
 
         // pull the relevant data out of the response
-        let gameData: BallDontLieData[] = apiResponseData.map(x => x.data.data);
-
-        // add the original response's data
-        gameData.push(...apiResponse.data.data);
+        gameData.push(...(apiResponseData.map(x => x.data.data)));
 
         // flatten the array of arrays
         let sznData: BallDontLieData[] = ([] as BallDontLieData[]).concat.apply([], gameData);
