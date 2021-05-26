e60b978befd671c66acac4e9cd8718364773b4bc
Asim <asimjbrown@gmail.com>
2021-05-26 14:39:26 -0500

    scheduling the game day check to run once a day at 6am

--- betItServer/app.ts
+++ betItServer/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
-
+import schedule from 'node-schedule';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
@@ -14,7 +14,7 @@ import fs from 'fs';
 import usersRouter from './routes/users.js';
 import sportsHandler from './routes/sports.js';
 import walletHandler from './routes/wallet_routes.js';
-import { wagerOps } from './database_connection/DatabaseOperations.js';
+import { sportOps, wagerOps } from './database_connection/DatabaseOperations.js';
 let accessLogStream = fs.createWriteStream(path.join(__dirname, '/logs/access.log'), { flags: 'a' })
 let app = express();
 
@@ -31,6 +31,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // listen to db events
 wagerOps.setUpSubscriber();
+
+// set up scheduler to check for nba games once a day at 6am central
+const gameDayJob = schedule.scheduleJob({hour: 6, minute: 0, tz: 'America/Chicago'}, function(){
+  sportOps.gameDayCheck();
+});
 
 // app.use('/', indexRouter);
 // app.use('/btc-handler', walletHandler);
