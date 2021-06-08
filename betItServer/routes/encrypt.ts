8f0374ff9b52eb72a853af8893bea5c47e343cbd
Asim <asimjbrown@gmail.com>
2021-06-08 10:23:05 -0500

    typing for encryption functions

--- betItServer/routes/encrypt.ts
+++ betItServer/routes/encrypt.ts
@@ -2,10 +2,10 @@ export {};
 import crypto from 'crypto';
 const algorithm = 'aes-256-gcm';
 // passphrase used to generate key
-const pwForSecretKey: string = process.env.ENCRYPTIONSECRETKEY ? process.env.ENCRYPTIONSECRETKEY : '';
+const pwForSecretKey: string = process.env.ENCRYPTIONSECRETKEY as string;
 const iv: Buffer = Buffer.alloc(16, 0); // Initialization vector.
 
-function encryptKey(key: any) {
+function encryptKey(key: string): string {
     // create the cipher using the key
     try {
         let cipher = crypto.createCipheriv(algorithm, pwForSecretKey, iv);
@@ -14,10 +14,11 @@ function encryptKey(key: any) {
         return encrypted;
     } catch (e) {
         console.log(e);
+        return '';
     }
 }
 
-function decryptKey(key: any): string {
+function decryptKey(key: string): string {
     let decipher = crypto.createDecipheriv(algorithm, pwForSecretKey, iv);
     let decrypted = decipher.update(key, 'hex', 'utf8');
     return decrypted;
