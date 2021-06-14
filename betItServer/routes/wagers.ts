54a7fe48a9877d34310f0ef8ef53f81ec01cf0c8
Asim <asimjbrown@gmail.com>
2021-06-13 23:42:12 -0700

    adding route to get wagers

--- betItServer/routes/wagers.ts
+++ betItServer/routes/wagers.ts
@@ -1,5 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import axios from'axios';
 import { userLogger, wagerLogger } from '../loggerSetup/logSetup.js';
-import { dbOps } from '../database_connection/DatabaseOperations.js';
+import { dbOps, wagerOps } from '../database_connection/DatabaseOperations.js';
 let router = express.Router();
+
+router.post('/get-wagers-by-game', async (req: Request, res: Response) => {
+    if (req.body.hasOwnProperty("gameId")) {
+        let wagers = await wagerOps.getWagersByGameId(req.body.gameId);
+        res.json(wagers);
+    }
+});
+
+export default router;
