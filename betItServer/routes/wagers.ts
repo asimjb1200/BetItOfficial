41007b9b6b5ca50cb2b25897faa0a892a8f02cc9
Asim <asimjbrown@gmail.com>
2021-06-15 00:45:00 -0700

    addtl route api protection and new route added

--- betItServer/routes/wagers.ts
+++ betItServer/routes/wagers.ts
@@ -5,9 +5,27 @@ import { dbOps, wagerOps } from '../database_connection/DatabaseOperations.js';
 let router = express.Router();
 
 router.post('/get-wagers-by-game', async (req: Request, res: Response) => {
-    if (req.body.hasOwnProperty("gameId")) {
+    if (req.body.hasOwnProperty("gameId") && typeof req.body.gameId == 'number') {
         let wagers = await wagerOps.getWagersByGameId(req.body.gameId);
-        res.json(wagers);
+        if (wagers.length > 0) {
+            res.status(200).json(wagers);
+        } else {
+            res.status(404).json([]);
+        }
+    } else {
+        res.status(400).send('invalid game id');
+    }
+});
+
+router.post('/add-fader-to-wager', async (req: Request, res: Response) => {
+    if (req.body.hasOwnProperty("wager_id") && req.body.hasOwnProperty("fader_address")) {
+        const faderAddr = req.body.fader_address
+        const wagerId = req.body.wager_id
+
+        // make the update to the wager
+        let updatedWager = await wagerOps.updateWager(wagerId, faderAddr);
+
+        return res.status(200).json(updatedWager);
     }
 });
 
